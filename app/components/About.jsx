45d2e21e4236ea881ea40e6856b375871aee8efd
MistaTwist <porlyp@gmail.com>
2016-05-13 16:25:01 +0100

    Added about content

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,8 +3,20 @@ var React = require('react');
 var About = (props) => {
   return (
     <div>
-      <h3>About</h3>
-      <p>Welcome to About.</p>
+      <h1 className="text-center">About</h1>
+      <p>This Weather App was built in React as part of the very detailed and useful "The Complete React Web Developer Course"course on Udemy.
+      </p>
+      <p>
+        Some of the technologies used to make this were:
+        <ul>
+          <li>
+            <a href="https://facebook.github.io/react">React</a> - Main js Framework.
+          </li>
+          <li>
+            <a href="http://openweathermap.org/">Open Weather Map</a> - Source of weather data.
+          </li>
+        </ul>
+      </p>
     </div>
   )
 };
